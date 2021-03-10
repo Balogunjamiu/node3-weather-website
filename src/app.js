f039aa77e500c1bde34a7f79b0f864bf0e50a48a
balogunjamiu49@gmail.com <balogunjamiu49@gmail.com>
2021-03-10 19:21:31 +0100

    REMOVE AN ERROR

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.get('/help', (req,res)=>{
         name: 'balogun jamiu'
     } )
 })
-app.get('/weather/address=', (req,res)=>{
+app.get('/weather', (req,res)=>{
     if (!req.query.address){
         return res.send({
             error:"you have not provided any location"
